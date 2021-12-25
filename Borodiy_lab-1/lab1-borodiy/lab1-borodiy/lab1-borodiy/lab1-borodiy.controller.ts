e9d7351a4b999de4ce3760183c31012669f017cb
jekaa20bor <o.borodii@student.ntu.edu.ua>
2021-12-26 00:37:32 +0200

    [lab1] - the task was solved

--- Borodiy_lab-1/lab1-borodiy/lab1-borodiy/lab1-borodiy/lab1-borodiy.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Контролер } з "@nestjs/common";
-
-@Контролер('lab1-borodiy')
-експорт клас Lab1БородійController {}
--- Borodiy_lab-1/lab1-borodiy/lab1-borodiy/lab1-borodiy/lab1-borodiy.service.ts
+++
@@ -1,4 +0,0 @@
-import { Injectable } з "@nestjs/common";
-
-@Ін'єкційні()
-експорт клас Lab1БородійСервіс {}
