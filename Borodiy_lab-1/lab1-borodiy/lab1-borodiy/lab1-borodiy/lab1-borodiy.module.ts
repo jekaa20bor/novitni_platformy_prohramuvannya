d58842dc10e7d40ca68958800f9d2e77d285d9d2
jekaa20bor <o.borodii@student.ntu.edu.ua>
2021-12-26 00:45:24 +0200

    [lab1] - the task was solved

--- Borodiy_lab-1/lab1-borodiy/lab1-borodiy/lab1-borodiy/lab1-borodiy.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } з "@nestjs/common";
-import { Lab1BorodiyController } від './lab1-borodiy.controller';
-import { Lab1BorodiyService } від './lab1-borodiy.service';
-
-@Модуль({
- контролери: [Лабораторія1БородійController],
- постачальники послуг: [Лаб1БородійСервіс]
-})
-експорт класу Lab1БородійМодула {}
