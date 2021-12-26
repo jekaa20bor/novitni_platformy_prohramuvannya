c56e8bdf5aece0de75cb9df18cb8b9a18fda4e0e
jekaa20bor <o.borodii@student.ntu.edu.ua>
2021-12-26 03:04:18 +0200

    [lab4] - the task was solved

--- project1/src/app.service.ts
+++ project1/src/app.service.ts
@@ -3,23 +3,12 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): Person {
-    const person = new Person("Serhii", 21, 70);
+    const person = new Person("OKSANA", 21, 70);
     return person;
   }
 }
 
-<<<<<<< HEAD
-  public compare(a : number, b : number, number : number) : number {
-    if (number % 2 == 0) {
-      if (a > b) {
-        return a;
-      } else if (a < b) {
-        return b;
-      } else return a;
-    } else if (number % 2 != 0) {
-      return a + b;
-    }
-=======
+
 export class Person {
   name : string;
   age : number;
@@ -29,6 +18,6 @@ export class Person {
     this.name = name;
     this.age = age;
     this.weight = weight;
->>>>>>> 134dc746ed3a0b1cd357acbcda2e4117a7b68810
+
   }
 }
